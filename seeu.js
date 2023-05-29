d55174470fbfadae2ec1697cc1d7ddf91bd81cd3
Nguyễn Minh Triết <trietnmgcs210344@fpt.edu.vn>
2023-05-29 08:51:45 +0700

    cancer

--- seeu.js
+++ seeu.js
@@ -1,17 +1,8 @@
-const http = require ( 'http' );
-const hostname = 'localhost' ;
-const port = 3000 ;
-
-const server = http.createServer((req, res) => {
-
-    res.statusCode = 200 ;
-    res.setHeader( "Content-Type" , "text /html" );
-    res.end(
-        "<h1>hello world</h1>"
-        
-    );
-});
-
-server.listen(port, hostname, () => {
-    console .log( `Server running at http://${hostname}:${port}/` );
-});
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+res.send('Hello, World!')
+})
+app.listen(3000, () => {
+console.log('Server started')
+})
